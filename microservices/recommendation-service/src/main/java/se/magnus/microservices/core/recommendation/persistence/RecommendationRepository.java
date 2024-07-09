bbe2d81c999162cd65c3bf4b3a76b2a0ce591ee5
Matvey Voropay <vorofpie@gmail.com>
2024-07-09 14:06:44 +0300

    Add new APIs and tests

--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,8 +1,7 @@
 package se.magnus.microservices.core.recommendation.persistence;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
   List<RecommendationEntity> findByProductId(int productId);
--- microservices/review-service/src/main/java/se/magnus/microservices/core/review/persistence/ReviewRepository.java
+++ microservices/review-service/src/main/java/se/magnus/microservices/core/review/persistence/ReviewRepository.java
@@ -1,9 +1,8 @@
 package se.magnus.microservices.core.review.persistence;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<ReviewEntity, Integer> {
 
